0290bf6d42326c22ab9407688c0eb6ea7d196fd7
mbeldozwew <51509569+mbeldozwew@users.noreply.github.com>
2020-06-05 14:52:23 +0700

    Update order.js

--- models/order.js
+++ models/order.js
@@ -22,7 +22,7 @@ const OrderSchema = new mongoose.Schema(
     address: String,
     status: {
       type: String,
-      default: "Not processed",
+      default: "Processing",
       enum: ["Not processed", "Processing", "Shipped", "Delivered", "Cancelled"] // enum means string objects
     },
     updated: Date,
